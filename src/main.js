70cf689533c5fb06ae8ab707b05cf92b0b28fd6e
Marcin Pacholarz <marrcin.pacholarz@gmail.com>
2019-06-15 13:54:21 +0200

    add eslint with standard JS config

--- src/main.js
+++ src/main.js
@@ -1,45 +1,43 @@
-import Phaser from 'phaser';
-import dejto from './skrpy';
+import Phaser from 'phaser'
 
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent: 'game',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'game',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
     }
-};
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+}
 
-var game = new Phaser.Game(config);
+window.game = new Phaser.Game(config)
 
 function preload () {
-    this.load.image('ship', 'assets/ship.png');
-    this.load.image('fire', 'assets/fire1.png');
-    dejto();
+  this.load.image('ship', 'assets/ship.png')
+  this.load.image('fire', 'assets/fire1.png')
 }
 
 function create () {
-    var particles = this.add.particles('fire');
+  var particles = this.add.particles('fire')
 
-    var emitter = particles.createEmitter({
-        speed: 50,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
+  var emitter = particles.createEmitter({
+    speed: 50,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD'
+  })
 
-    var ship = this.physics.add.image(400, 100, 'ship');
+  var ship = this.physics.add.image(400, 100, 'ship')
 
-    ship.setVelocity(100, 200);
-    ship.setBounce(1, 1);
-    ship.setCollideWorldBounds(true);
+  ship.setVelocity(100, 200)
+  ship.setBounce(1, 1)
+  ship.setCollideWorldBounds(true)
 
-    emitter.startFollow(ship);
+  emitter.startFollow(ship)
 }
--- src/skrpy.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-    console.log('siemano z innego miejsca');
-}
